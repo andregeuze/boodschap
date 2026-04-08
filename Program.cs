c755d9b42c750105c7772aec590d56b242a771dc
André Geuze <andregeuze@users.noreply.github.com>
2026-04-08 18:36:00 +0200

    Refactor error handling configuration and streamline HTTP request pipeline in Program.cs

--- Program.cs
+++ Program.cs
@@ -9,14 +9,6 @@ builder.Services.AddRazorComponents()
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
 // With this in place, NPM's X-Forwarded-Proto: https header tells the app
 // the connection is already HTTPS, so UseHttpsRedirection() won't redirect anything.
 app.UseForwardedHeaders(new ForwardedHeadersOptions
@@ -24,9 +16,13 @@ app.UseForwardedHeaders(new ForwardedHeadersOptions
     ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
 });
 
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
 app.UseAntiforgery();
 app.UseStaticFiles();
-
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
